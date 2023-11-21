3e63889118e370d235687b3341f594fd31da2a77
DavidHP <maritotupapasito@gmail.com>
2023-11-20 20:00:26 -0600

    Se agrega el proyecto

---
+++ manager.py
@@ -0,0 +1,13 @@
+# Limpieza, Entrenamiento, Predicción.
+# Crear script de limpieza con clases, al final devuelve el csv limpio localmente.
+# Se crea un script central llamado manager.
+# API crea puertos, hay diferentes puertos: recibimiento, dando, e intercambio.
+# Librería para ordenar .Tree
+
+# GET, POST, PUT, DELETE.
+
+print("Limpieza")
+
+print("Entrenamiento")
+
+print("Predicción")
